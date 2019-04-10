28a67e3768720418bf4561057af49a8aee87c41b
acr <acarbey15@gmail.com>
2019-04-10 21:06:58 +0300

    yaz

---
+++ precache-manifest.a683619457b5e43fb91cbf1816c5b84e.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "46f121b2bfcd054023de",
+    "url": "/static/js/main.b5bcfc3c.chunk.js"
+  },
+  {
+    "revision": "dce88eeb2017be04c447",
+    "url": "/static/js/2.837bd388.chunk.js"
+  },
+  {
+    "revision": "46f121b2bfcd054023de",
+    "url": "/static/css/main.11d4044b.chunk.css"
+  },
+  {
+    "revision": "98301df43aaba5295d662af1625ff2c9",
+    "url": "/index.html"
+  }
+];
